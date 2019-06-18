7066f4c1c5e2130dc748a66a2699568794bcc0fc
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-06-18 20:35:08 +0200

    define type metadata for properties

--- src/constants/symbols.ts
+++ src/constants/symbols.ts
@@ -4,3 +4,4 @@ export const LOCKS = Symbol("LOCKS");
 export const INHERIT_HANDLERS = Symbol("INHERIT_HANDLERS");
 export const PARENTS = Symbol("PARENTS");
 export const QUERIES = Symbol("QUERIES");
+export const PROPERTY_TYPES = Symbol("PROPERTY_TYPES");
