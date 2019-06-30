3462f27f82d489d1ef4476a6b5f4792a49289df6
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-06-30 14:53:32 +0200

    reduce complexity of defaultAttributeParser and -Serializer

--- src/decorators/property-decorators/prop.ts
+++ src/decorators/property-decorators/prop.ts
@@ -43,24 +43,48 @@ export function defaultPropertyAttributeName(property: string) {
   return toKebapCase(property);
 }
 
+export const defaultParseMap: Record<string, {parser: (val: any, cls, prop, type) => any, serializer: (val: any, cls, prop, type) => any}> = {};
+export function registerParser(type: Function, parser: (val: any, cls, prop, type) => any, serializer: (val: any, cls, prop, type) => any) {
+  defaultParseMap[type.name] = {
+    parser,
+    serializer
+  };
+}
+export function getParser(type: Function) {
+  return defaultParseMap[type.name];
+}
+
+registerParser(String,
+  val => val != false ? val : undefined,
+  val => val
+);
+registerParser(Number,
+  val => val != false ? +val : undefined,
+  val => `${val}`
+);
+registerParser(Boolean,
+  val => !!val && val != "false",
+  val => val
+);
+registerParser(Date,
+  val => val ? (val as Date).toISOString() : undefined,
+  val => val ? new Date(val as string) : undefined
+);
+registerParser(RegExp,
+  val => val ? new RegExp(val as string) : undefined,
+  val => val ? (val as RegExp).source : undefined
+);
+
 export function defaultAttributeSerializer(val: any, cls: any, prop: string | symbol, type: Function) {
-  switch(type.name) {
-    case "String": return val;
-    case "Number": return `${val}`;
-    case "Boolean": return val;
-    case "Date": return val ? (val as Date).toISOString() : false;
-    case "RegExp": return val ? (val as RegExp).source : false;
-  }
+  const {serializer} = getParser(type);
+  if (serializer) return serializer(val, cls, prop, type);
+  throw new TypeError(`No serializer registered for type ${type.name}.`);
 }
 
 export function defaultAttributeParser(val: string | boolean, cls: any, prop: string | symbol, type: Function) {
-  switch(type.name) {
-    case "String": return val != false ? val : undefined;
-    case "Number": return val != false ? +val : undefined;
-    case "Boolean": return !!val && val != "false";
-    case "Date": return val ? new Date(val as string) : undefined;
-    case "RegExp": return val ? new RegExp(val as string) : undefined;
-  }
+  const {parser} = getParser(type);
+  if (parser) return parser(val, cls, prop, type);
+  throw new TypeError(`No parser registered for type ${type.name}.`);
 }
 
 /**
