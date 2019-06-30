d3a22272c4b3a97833a4b0c52c74eb786f0581fd
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-06-30 15:00:12 +0200

    move attribute parsing into new file

--- src/decorators/property-decorators/prop.ts
+++ src/decorators/property-decorators/prop.ts
@@ -1,9 +1,9 @@
+import "reflect-metadata";
 import {toKebapCase} from "@/util";
 import {addExtension, ComponentExtension} from "@/component/extension";
 import {Constructor, Webcomponent} from "@/types";
-import "reflect-metadata";
-import {NotifyEvent} from "@/properties/notify";
 import {PROPERTY_TYPES, Stages} from "@/constants";
+import {NotifyEvent, defaultAttributeParser, defaultAttributeSerializer} from "@/properties";
 
 /**
  * @typedef PropertyOptions
@@ -41,50 +41,6 @@ export const DefaultPropertyOptions: PropertyOptions = {
 
 export function defaultPropertyAttributeName(property: string) {
   return toKebapCase(property);
-}
-
-export const defaultParseMap: Record<string, {parser: (val: any, cls, prop, type) => any, serializer: (val: any, cls, prop, type) => any}> = {};
-export function registerParser(type: Function, parser: (val: any, cls, prop, type) => any, serializer: (val: any, cls, prop, type) => any) {
-  defaultParseMap[type.name] = {
-    parser,
-    serializer
-  };
-}
-export function getParser(type: Function) {
-  return defaultParseMap[type.name];
-}
-
-registerParser(String,
-  val => val != false ? val : undefined,
-  val => val
-);
-registerParser(Number,
-  val => val != false ? +val : undefined,
-  val => `${val}`
-);
-registerParser(Boolean,
-  val => !!val && val != "false",
-  val => val
-);
-registerParser(Date,
-  val => val ? (val as Date).toISOString() : undefined,
-  val => val ? new Date(val as string) : undefined
-);
-registerParser(RegExp,
-  val => val ? new RegExp(val as string) : undefined,
-  val => val ? (val as RegExp).source : undefined
-);
-
-export function defaultAttributeSerializer(val: any, cls: any, prop: string | symbol, type: Function) {
-  const {serializer} = getParser(type);
-  if (serializer) return serializer(val, cls, prop, type);
-  throw new TypeError(`No serializer registered for type ${type.name}.`);
-}
-
-export function defaultAttributeParser(val: string | boolean, cls: any, prop: string | symbol, type: Function) {
-  const {parser} = getParser(type);
-  if (parser) return parser(val, cls, prop, type);
-  throw new TypeError(`No parser registered for type ${type.name}.`);
 }
 
 /**
