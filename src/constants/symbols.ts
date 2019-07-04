5b5d7944039837649bf2e8800b8f88df42e2f920
Merlin Reichwald <merlin.reichwald@gmail.com>
2019-07-04 02:10:29 +0200

    add symbol for model metadata

--- src/constants/symbols.ts
+++ src/constants/symbols.ts
@@ -5,3 +5,4 @@ export const INHERIT_HANDLERS = Symbol("INHERIT_HANDLERS");
 export const PARENTS = Symbol("PARENTS");
 export const QUERIES = Symbol("QUERIES");
 export const PROPERTY_TYPES = Symbol("PROPERTY_TYPES");
+export const MODELS = Symbol("MODELS");
